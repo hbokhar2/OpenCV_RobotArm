4eaf82138281cb946476807f1ec1952b276fbb99
B0LDOW <harrisbokhari@outlook.com>
2024-11-27 16:34:59 -0700

    sub

--- src/main.py
+++ src/main.py
@@ -1,59 +1,68 @@
 import numpy as np
-import cv2
+import cv2 as cv
 
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(1)
 
-threshold = 30
+while(1):
 
-while (1):
     _,frame = cap.read()
 
-    red = frame[:,:,2] # : means all [row, column, layer] third layer
-    green = frame[:,:,1]  # : means all [row, column, layer] second layer
-    blue = frame[:,:,0]  # : means all [row, column, layer] first layer
-
-    # make value a 16 bit that can be positive or negative
-    red_only = np.int16(red) - np.int16(green) - np.int16(blue)
-
-    # numbers to achieve valid matrix ranging from 0 to 255
-    red_only[red_only < threshold] = 0 # sets #'s lower than 0 to 0
-    red_only[red_only >= threshold] = 255 # sets #'s greater than 255 down to 255
-
-
-    cv2.imshow('Bokhari-RGB',frame)
-    cv2.imshow('Bokhari-Red',red)
-    #cv2.imshow('RO-Green',green)
-    #cv2.imshow('RO-Blue',blue)
-
-    red_only = np.uint8(red_only)
-
-    cv2.imshow('Bokhari-Red Only', red_only)
-
-    k = cv2.waitKey(5) # number is ms delay to check if
-    # the number 27 is the ASCII control character value for the esc key
-
+    gray_image = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    cv.imshow('background', gray_image)
+    k = cv.waitKey(5)
     if k == 27:
         break
 
-#finding the center of the object for x coordinate
-column_sums = np.matrix(np.sum(red_only, 0)) # 0 sums the columns
+while(1):
+    _,frame = cap.read()
+
+    gray_image_2 = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    cv.imshow('background', gray_image_2)
+
+    difference = np.absolute(np.matrix(np.int16(gray_image)) - np.matrix(np.int16(gray_image_2)))
+    difference[difference > 255] = 255
+
+    difference = np.uint8(difference)
+
+    cv.imshow('difference', difference)
+
+    k = cv.waitKey(5)
+    if k == 27:
+        break
+
+#Center of object in X coordinate
+column_sums = np.matrix(np.sum(difference,0))
 column_numbers = np.matrix(np.arange(640))
-column_mult = np.multiply(column_sums, column_numbers) #element wise multiplication
+column_mult = np.multiply(column_sums, column_numbers)
 column_total = np.sum(column_mult)
-total_total = np.sum(np.sum(red_only)) #sums all values in the image
-red_column_location = column_total/total_total
-print('Red column ("X") location: ', red_column_location)
+total_total = np.sum(np.sum(difference))
+difference_column_location = column_total/total_total
 
-#finding the center of the object for Y coordinate
-row_sums = np.matrix(np.sum(red_only, 1))
-#row_sums = row_sums.transpose()
+print('Object column ("X") location: ', difference_column_location)
+
+#Center of object in Y coordinate
+row_sums = np.matrix(np.sum(difference,1))
+row_sums = row_sums.transpose()
 row_numbers = np.matrix(np.arange(480))
-row_mult = np.multiply(row_sums, row_numbers)
+row_mult = np.multiply(row_sums,row_numbers)
 row_total = np.sum(row_mult)
-red_row_location = row_total / total_total
-print('Red row ("Y") location: ', red_row_location)
+difference_row_location = row_total/total_total
 
-red_only = np.uint8(red_only) # ensure red_only is a unsigned interger
+print('Object column ("Y") location: ', difference_row_location)
 
-cv2.destroyAllWindows() #closes openned python windows
- 
+#Crosshairs
+color = (255, 255, 255)  # White
+thickness = 1
+
+diff_x_location = int(difference_column_location)
+diff_y_location = int(difference_row_location)
+
+difference = cv.line(difference, (diff_x_location, 0), (diff_x_location, 480), color, thickness)
+difference = cv.line(difference, (0, diff_y_location), (640, diff_y_location), color, thickness)
+
+cv.imshow('difference', difference)
+
+#Wait and release resources
+cv.waitKey(0)
+cap.release()
+cv.destroyAllWindows()
